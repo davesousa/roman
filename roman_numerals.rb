f563afd96d7a8d21f6295b224660554ce9c224c8
Dave Sousa <davejsousa@gmail.com>
2016-03-02 19:35:58 -0500

    roman update

---
+++ roman_numerals.rb
@@ -0,0 +1,35 @@
+@roman = {
+  M: 1000,
+  D: 500,
+  C: 100,
+  L: 50,
+  X: 10,
+  v: 5,
+  I: 1
+}
+
+def to_roman(num)
+  numeral = " "
+
+  @roman.each do |key, value|
+      times = num / value
+      if times >= 1
+        remainder = num % value
+        keyletter = key.to_s * times
+        numeral << keyletter
+        num = num - (value * times)
+      end
+  end
+  return numeral
+end
+
+
+
+
+# Drive code... this should print out trues.
+puts to_roman(1)
+puts to_roman(1) == "I"
+puts to_roman(3) == "III"
+puts to_roman(6) == "VI"
+
+# TODO: what other cases could you add to ensure your to_roman method is working?
